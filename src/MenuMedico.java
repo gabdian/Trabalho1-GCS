79261ae13e61ffcd879c23d27be546ebd0e3150f
Lucas Berr <lucastberr@gmail.com>
2026-05-12 21:15:49 -0300

    Adicionando funcao que pega o Index que o usuario selecionou no console

--- src/MenuMedico.java
+++ src/MenuMedico.java
@@ -12,47 +12,47 @@ import model.TipoExame;
 public class MenuMedico {
     public static void novaAutorizacao() {
         // Imprime a tela inicial para fazer a Nova Autorizacao
-        System.out.println("\n === NOVA AUTORIZAÇÃO ===\n");
+        System.out.println("\n ====== NOVA AUTORIZAÇÃO ======\n");
 
         Scanner scanner = new Scanner(System.in);
         
         // 1) Pegar a data em que o cadastro da autorizacao esta sendo realizado
         LocalDate dataCadastro = LocalDate.now();
         
-        System.out.println("Data da Autorização: " + dataCadastro);
         // 2) Interagir entre todos os elementos que ja existem que sao medicos e printar na tela
         // para o usuário selecionar qual medico ele deseja. (apenas 1)
         Medico medicoSelecionado = selecionaMedico(scanner);
 
         // 3) Interagir entre todos os elementos que ja existem que sao pacientes e printar na tela
         // para o usuário selecionar qual pacientes ele deseja. (apenas 1)
-        List<Paciente> pacientes = DataStore.getPacientes();
-        System.out.println("Pacientes: "+ pacientes);
-
+        
         // 4) Interagir entre todos os elementos que ja existem que sao exame e printar na tela
         // para o usuário selecionar qual pacientes ele deseja. (apenas 1)
-        TipoExame[] exames = TipoExame.values();
-        System.out.println("Exames: " + Arrays.toString(exames));
-
+        
     }
 
-    public static Medico selecionaMedico(Scanner scanner) {
+    private static Medico selecionaMedico(Scanner scanner) {
         List<Medico> medicos = DataStore.getMedicos();
         
-        System.out.println("Indice | Médico");
+        int indice = getIndexSelecionado(scanner, medicos, "Médico");
+        return medicos.get(indice);
+    }
 
-        for (int i = 0; i < medicos.size(); i++) {
-            System.out.println("  " + (i + 1) + "    - " + medicos.get(i));
+    private static <T> int getIndexSelecionado(Scanner scanner, List<T> lista, String nomeEntidade) {
+        System.out.println("Indice | " + nomeEntidade);
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("  " + (i + 1) + "    - " + lista.get(i));
         }
 
-        System.out.print("Digite o indice do Médico: ");
-        int opcaoMedico = scanner.nextInt();
-        while (opcaoMedico < 1 ||
-                opcaoMedico >= medicos.size()) {
+        System.out.print("Digite o indice do "+ nomeEntidade +": ");
+        int opcao = scanner.nextInt();
+        while (opcao < 1 ||
+                opcao > lista.size()) {
             System.out.print("Digite um indice válido: ");
-            opcaoMedico = scanner.nextInt();
+            opcao = scanner.nextInt();
         }
-        Medico medicoSelecionado = medicos.get((opcaoMedico-1));
-        return medicoSelecionado;
+
+        return (opcao - 1);
     }
 }
