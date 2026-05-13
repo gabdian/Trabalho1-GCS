25d82c3674f8d8be471ef56d26e5748229f633fc
GABRIEL DIAN DE OLIVEIRA <gabriel.dian@edu.pucrs.br>
2026-05-13 16:10:36 -0300

    corrigindo bugs nos métodos de buscar paciente e buscar médico por nome

--- src/MenuAdmin.java
+++ src/MenuAdmin.java
@@ -1,5 +1,7 @@
 import data.DataStore;
 import model.Autorizacao;
+import model.Medico;
+import model.Paciente;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -59,24 +61,104 @@ public class MenuAdmin {
         }
     }
 
-    public static void buscarPacientePorNome(String nome) {
+    /**
+     * Valida e adiciona um paciente para a lista
+     * @param pacientes Lista de destino
+     * @param nome Nome do paciente a ser cadastrado
+     */
 
-        String nomeBusca = null;
+    public static void incluirPaciente(List<Paciente> pacientes,
+                                       String nome) {
 
+        boolean existe = false;
+
+        // Verifica se o nome já consta na lista de pacientes
         for (Paciente p : pacientes) {
-            if (p.nome.equals(nomeBusca)) {
-                System.out.println("Paciente encontrado: " + p.nome);
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                existe = true;
             }
+        }
+
+        // Validações de regras de negócio antes da inserção
+        if (nome.isEmpty()) {
+            System.out.println("Nome inválido.");
+        } else if (nome.length() < 3) {
+            System.out.println("Nome muito curto.");
+        } else if (existe) {
+            System.out.println("OBS: Paciente já fora cadastrado.");
+        } else {
+            //Como Usuário é abstrata, instancia-se a subclasse concreta Paciente
+            Paciente novoPaciente;
+            novoPaciente = new Paciente(+1, nome, "");
+            pacientes.add(novoPaciente);
+
+            System.out.println("Paciente incluído com sucesso.");
         }
     }
 
-    public static void buscarMedicoPorNome(String nome) {
-        String nomeBusca = null;
+    /**
+     * Valida e adiciona um médico para a lista
+     * @param medicos Lista de destino
+     * @param nome Nome do médico a ser cadastrado
+     */
+    public static void incluirMedico(List<Medico> medicos,
+                                     String nome) {
 
+        boolean existe = false;
+
+        //Verifica se o nome já existe na lista de medicos
         for (Medico m : medicos) {
-            if (m.nome.equals(nomeBusca)) {
-                System.out.println("Médico encontrado: " + m.nome);
+            if (m.getNome().equalsIgnoreCase(nome)) {
+                existe = true;
             }
+        }
+
+        //Validações de regras de negócio antes da inserção
+        if (nome.isEmpty()) {
+            System.out.println("Nome inválido.");
+        } else if (nome.length() < 3) {
+            System.out.println("Nome muito curto.");
+        } else if (existe) {
+            System.out.println("OBS: Médico já fora cadastrado.");
+        } else {
+            // instanciação da subclasse concreta Médico
+            Medico novoMedico;
+            novoMedico = new Medico(1, nome, "");
+            medicos.add(novoMedico);
+
+            System.out.println("Médico incluído com sucesso.");
+        }
+    }
+
+    /**
+     * Procura um paciente na lista pelo nome
+     * @param pacientes Lista onde a busca será executada
+     * @param nome Critério de busca
+     */
+    public static void buscarPacientePorNome(List<Paciente> pacientes, String nome) {
+
+        //Percorre a lista comparando o parâmetro 'nome' com o atributo do objeto
+        for (Paciente p : pacientes) {
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                System.out.println("Paciente encontrado: " + p.getNome());
+            }
+            System.out.println("Paciente não existe" + nome);
+        }
+    }
+
+    /**
+     * Procura um médico na lista pelo nome
+     * @param medicos Lista onde a busca será executada
+     * @param nome Critério de busca
+     */
+    public static void buscarMedicoPorNome(List<Medico> medicos, String nome) {
+
+        //Percorre a lista comparando o parâmetro 'nome' com o atributo do objeto
+        for (Medico m : medicos) {
+            if (m.getNome().equalsIgnoreCase(nome)) {
+                System.out.println("Médico encontrado: " + m.getNome());
+            }
+            System.out.println("Médico não existe" + nome);
         }
     }
 }
